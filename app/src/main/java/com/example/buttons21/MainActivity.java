b8f0ce8c9adb5f0471a38d145a038f8c2c22f3b3
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-12 22:18:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/buttons21/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.buttons21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int myNumber = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn(View view) {
+        myNumber++;
+    }
+}
